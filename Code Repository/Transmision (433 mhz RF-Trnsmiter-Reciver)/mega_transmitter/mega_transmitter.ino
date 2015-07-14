d30c3ac86dd249e125f16cc132f70d10805a2af5
Adnan Shafique <ad11105033@gmail.com>
2015-07-14 16:25:04 +0500

    :+1: working Grid n RF-Transmission
    :+1: working Grid n RF-Transmission

---
+++ Code Repository/Transmision (433 mhz RF-Trnsmiter-Reciver)/mega_transmitter/mega_transmitter.ino
@@ -0,0 +1,61 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+int serRead;
+void setup()
+{
+    Serial.begin(9600);	  // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+serRead=  Serial.read();
+  if(serRead==49)
+  {
+        const char *msg = "R";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+        Serial.print(serRead);
+  }
+   if(serRead==50)
+  {
+        const char *msg = "L";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+        Serial.print(serRead);
+  }
+   if(serRead==51)
+  {
+        const char *msg = "F";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+        Serial.print(serRead);
+  }
+   if(serRead==52)
+  {
+        const char *msg = "B";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+        Serial.print(serRead);
+  }
+  
+   if(serRead==48)
+  {
+        const char *msg = "S";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+        Serial.print(serRead);
+  }
+  else
+ {
+    const char *msg = "W";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+        Serial.print(serRead);
+ }
+}
+//////////////////////////////
+
